f93d4f102bd7181b681979e6eb0b52d07caed01c
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-08-15 23:40:11 +0900

    aaa

--- src/test.ts
+++ src/test.ts
@@ -4,36 +4,3 @@ interface User {
 }
 
 
-const test = (user:User) => {
-     
-}
-
-const tes = {
-    name:'kjkjk',
-    age:33,
-    gender:'man'
-}
-
-const l:User = {
-    name:'kkk',
-    age:88,
-    gender:'kjkj'
-}
-const jjj:User = tes
-test(tes)
-
-const kjkj:string = 'kkk'
-const ads:string = 777
-const mmm:number = 33
-
-const assertNever = () => {
-    return ''
-}
-sdfあsd
-const tesdfad = () => {
-    return 2333
-}dファファsd
-sdfasdf
-djkfhasdjfg
-let aa = 33
-const asdf = 878
